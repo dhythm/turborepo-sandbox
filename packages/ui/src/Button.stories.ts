4fc97e0dd78f4fafcf79e027dd4dfdddc3c2da31
dhythm <yuta.okada.20@gmail.com>
2025-03-15 00:04:35 +0900

    feat(@repo/ui): move story into the package

---
+++ packages/ui/src/Button.stories.ts
@@ -0,0 +1,19 @@
+import type { Meta, StoryObj } from "@storybook/react";
+import { Button } from "./button";
+
+// More on how to set up stories at: https://storybook.js.org/docs/writing-stories#default-export
+const meta = {
+  title: "Example/Button",
+  component: Button,
+  tags: ["autodocs"],
+} satisfies Meta<typeof Button>;
+
+export default meta;
+type Story = StoryObj<typeof meta>;
+
+export const Primary: Story = {
+  args: {
+    appName: "Button",
+    children: "I am a primary button.",
+  },
+};
