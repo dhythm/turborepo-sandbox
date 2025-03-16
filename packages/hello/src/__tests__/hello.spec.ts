f645ed405919580edb406a37a6410df612ba2687
dhythm <yuta.okada.20@gmail.com>
2025-03-17 00:59:03 +0900

    chore: resolve error for tests

--- packages/hello/src/__tests__/hello.spec.ts
+++ packages/hello/src/__tests__/hello.spec.ts
@@ -1,7 +1,7 @@
 import { expect, test } from "vitest";
 import { hello } from "../hello";
 
-test("adds 1 + 2 to equal 3", () => {
+test("say hello", () => {
   expect(hello("John")).toBe("Hello, John!");
   expect(hello("Jane")).toBe("Hello, Jane!");
 });
