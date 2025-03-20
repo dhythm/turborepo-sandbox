ab081132614be8d88c181e5bc7d856db309666e8
dhythm <yuta.okada.20@gmail.com>
2025-03-20 19:14:02 +0900

    feat(vite-app): pass vitest + RTL

--- apps/vite-app/src/__tests__/App.spec.tsx
+++ apps/vite-app/src/__tests__/App.spec.tsx
@@ -2,5 +2,7 @@ import { render, screen } from "@testing-library/react";
 import App from "../App";
 test("renders hello message", () => {
   render(<App />);
-  expect(screen.getByText("Hello, Vite!")).toBeInTheDocument();
+  expect(
+    screen.getByRole("heading", { name: "Vite + React" }),
+  ).toBeInTheDocument();
 });
