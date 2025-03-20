74fb55226938f7eec94fa69f325709ca871a7b81
dhythm <yuta.okada.20@gmail.com>
2025-03-20 17:13:20 +0900

    fix(hello): resolve a failed test by using jsdom

--- packages/hello/vitest.config.ts
+++ packages/hello/vitest.config.ts
@@ -11,5 +11,6 @@ export default defineConfig({
   test: {
     include: ["src/**/*.{test,spec}.{js,mjs,cjs,ts,mts,cts,jsx,tsx}"],
     setupFiles: ["./vitest.setup.ts"],
+    environment: "jsdom", // fetch is not available in node
   },
 });
